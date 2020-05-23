19166b96f5d6121c5b5535c07cb0e91b007e17bb
yanlibo2021 <yanlibo2021@126.com>
2020-05-23 22:11:32 +0800

    add src

---
+++ components/PageTitle.vue
@@ -0,0 +1,52 @@
+<template>
+    <el-row justify="space-between">
+        <el-col :span="12" class="page-title">
+            <div>{{title}}</div>
+        </el-col>
+        <el-col :span="12">
+            <el-breadcrumb separator-class="el-icon-arrow-right">
+                <template v-for="breadcrumb in breadcrumbs">
+                    <el-breadcrumb-item><i :class="breadcrumb.icon"></i>{{breadcrumb.title}}</el-breadcrumb-item>
+                </template>
+            </el-breadcrumb>
+        </el-col>
+    </el-row>
+</template>
+
+<script>
+
+    export default {
+        components: {},
+        created() {
+            let name = this.$route.name;
+            let breads = this.$store.getters.getAllPathByName(name)
+            this.breadcrumbs = breads;
+            this.title=this.$store.getters.getCurrentPathTitle(name)
+        },
+        asyncData() {
+
+        },
+        mounted() {
+
+        },
+        methods: {},
+        data() {
+            return {
+                breadcrumbs: [],
+                title:null
+            }
+        },
+    };
+</script>
+
+<style scoped lang="scss">
+    .el-breadcrumb{
+        line-height: 37px;
+        position: absolute;
+        right: 10px;
+    }
+    .page-title{
+        font-size: 25px;
+        margin-bottom: 10px;
+    }
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,61 @@
+<template>
+    <el-container>
+        <el-header>
+            <Header/>
+        </el-header>
+
+        <el-container>
+            <SideMenu/>
+            <el-main>
+                <nuxt/>
+            </el-main>
+        </el-container>
+    </el-container>
+</template>
+
+<script>
+    import Header from "~/components/Header";
+    import SideMenu from '@/components/SideMenu.vue';
+    export default {
+        components: {
+            Header,
+            SideMenu
+        },
+        data() {
+            return {};
+        },
+        mounted() {
+            console.log("default-layout-mounted");
+        }
+    };
+</script>
+
+<style scoped>
+    .el-aside {
+        background-color: #545c64;
+    }
+
+    .el-container {
+        height: 100%;
+    }
+
+    .el-header {
+        padding: 0;
+    }
+
+    .el-main {
+        height: 100%;
+        padding: 0;
+        background-color: #F2F6FC;
+    }
+
+    .el-menu {
+        border-right: 0px;
+    }
+
+    .sys-menu-title {
+        padding: 20px;
+        background-color: #545c64;
+        color: white;
+    }
+</style>
