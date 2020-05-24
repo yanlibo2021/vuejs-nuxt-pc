84c4a41f85b3d59ea81e2fcf4d25b4e412c7444d
yanlibo2021 <yanlibo2021@126.com>
2020-05-24 09:16:51 +0800

    add src

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,20 +6,20 @@ export default {
     head: {
         title: process.env.npm_package_name || "",
         meta: [
-            {charset: "utf-8"},
-            {name: "viewport", content: "width=device-width, initial-scale=1"},
+            { charset: "utf-8" },
+            { name: "viewport", content: "width=device-width, initial-scale=1" },
             {
                 hid: "description",
                 name: "description",
                 content: process.env.npm_package_description || ""
             }
         ],
-        link: [{rel: "icon", type: "image/x-icon", href: "/favicon.ico"}]
+        link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
     },
     /*
      ** Customize the progress-bar color
      */
-    loading: {color: "#fff"},
+    loading: { color: "#fff" },
     /*
      ** Global CSS
      */
@@ -31,6 +31,7 @@ export default {
     /*
      ** Plugins to load before mounting the App
      */
+    // plugins: ["@/plugins/element-ui", { src: '@/plugins/mock/mock-server', ssr: true }],
     plugins: ["@/plugins/element-ui"],
     /*
      ** Nuxt.js modules
@@ -45,6 +46,15 @@ export default {
          ** You can extend webpack config here
          */
         extend(config, ctx) {
+
+            if (ctx.isDev && ctx.isClient) {
+                //console.log("if dev")
+                // config.devServer = {
+                //     before: require('./mock/mock-server.js')
+                // }
+            } else {
+                //console.log("else product");
+            }
         }
     }
 };
